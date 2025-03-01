443df1eb0ae0dc781982f9c8ad2d4f52aea0a221
Jezabel Velasquez <153011111+ProfJeza@users.noreply.github.com>
2025-03-01 14:05:01 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,15 +4,13 @@ const multer = require('multer');
 const path = require('path');
 const { parseXML } = require('./parser'); // Esta es la línea correcta
 const fs = require('fs');
-const path = require('path');
+const uploadDir = './uploads'; 
 
 // Crear la carpeta uploads si no existe
-const uploadDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
     console.log("📂 Carpeta 'uploads' creada");
 }
-
 // Crear la aplicación de Express
 const app = express();
 const PORT = 5000;
